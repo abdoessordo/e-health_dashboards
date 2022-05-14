e868c749d9263c84913114bce3f7cfae7252db7c
abdoessordo <abdoessordo01@gmail.com>
2022-05-14 17:02:56 +0100

    search bar done

--- script/messages.js
+++ script/messages.js
@@ -14,3 +14,14 @@ show_conv = (id) => {
         document.querySelector(".recent-messages").classList.add("hide")
     }
 }
+const all_contacts = document.querySelectorAll(".chat-row")
+search_bar = (value) => {
+    const all_chats = document.querySelector(".recent-messages-body .all-chats")
+    all_chats.innerHTML = ''
+    all_contacts.forEach(contact => {
+        let contact_name = contact.querySelector(".chat-details > h3").innerHTML.toLocaleLowerCase()
+        if (contact_name.includes(value.toLocaleLowerCase())) {
+            all_chats.appendChild(contact)
+        }
+    })
+}
